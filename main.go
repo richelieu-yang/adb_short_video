7e94781986c9ca8659ba09e36b41ff28b034db08
Richelieu <richelieu042@gmail.com>
2026-02-04 09:18:17 +0800

    init

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 func main() {
-	os.Args = []string{"sv" /*"--addr", "127.0.0.1:5555",*/, "--clean", "--verbose"}
+	os.Args = []string{"sv", "--addr", "127.0.0.1:5555", "--clean", "--verbose"}
 	if err := cmd.Execute(); err != nil {
 		panic(err)
 	}
